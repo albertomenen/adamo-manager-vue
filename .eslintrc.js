a26928b7c2bdca7f4b699a2926384f3ce452c855
Javier Martinez <enriquejavier_21@hotmail.com>
2021-08-31 14:56:30 -0400

    Formateo lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,123 +1,123 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // allow async-await
-    "generator-star-spacing": "off",
+    'generator-star-spacing': 'off',
     // allow paren-less arrow functions
-    "arrow-parens": "off",
-    "one-var": "off",
-    "import/extensions": "off",
-    "import/no-unresolved": "off",
-    "import/no-extraneous-dependencies": "off",
-    "prefer-promise-reject-errors": "off",
-    semi: ["warn", "never"],
-    "space-before-function-paren": [
-      "warn",
+    'arrow-parens': 'off',
+    'one-var': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'prefer-promise-reject-errors': 'off',
+    semi: ['warn', 'never'],
+    'space-before-function-paren': [
+      'warn',
       {
-        anonymous: "always",
-        named: "always",
-        asyncArrow: "always",
-      },
+        anonymous: 'always',
+        named: 'always',
+        asyncArrow: 'always'
+      }
     ],
-    "space-before-blocks": "warn",
+    'space-before-blocks': 'warn',
     // js
-    "keyword-spacing": ["warn", { before: true, after: true }],
-    "space-infix-ops": "warn",
+    'keyword-spacing': ['warn', { before: true, after: true }],
+    'space-infix-ops': 'warn',
     indent: [
-      "warn",
+      'warn',
       2,
       {
         SwitchCase: 1,
-        ignoredNodes: ["TemplateLiteral"],
-        ignoreComments: true,
-      },
+        ignoredNodes: ['TemplateLiteral'],
+        ignoreComments: true
+      }
     ],
-    quotes: ["warn", "single", { allowTemplateLiterals: true }],
-    "linebreak-style": 0,
-    "no-trailing-spaces": "warn",
-    "no-empty": ["warn", { allowEmptyCatch: true }],
-    "key-spacing": ["warn", { afterColon: true }],
-    "eol-last": ["warn", "always"],
-    "object-curly-spacing": ["warn", "always"],
-    "object-curly-newline": "warn",
-    "no-multi-spaces": "warn",
-    "comma-dangle": ["warn", "never"],
-    "comma-spacing": [
-      "warn",
+    quotes: ['warn', 'single', { allowTemplateLiterals: true }],
+    'linebreak-style': 0,
+    'no-trailing-spaces': 'warn',
+    'no-empty': ['warn', { allowEmptyCatch: true }],
+    'key-spacing': ['warn', { afterColon: true }],
+    'eol-last': ['warn', 'always'],
+    'object-curly-spacing': ['warn', 'always'],
+    'object-curly-newline': 'warn',
+    'no-multi-spaces': 'warn',
+    'comma-dangle': ['warn', 'never'],
+    'comma-spacing': [
+      'warn',
       {
         before: false,
-        after: true,
-      },
+        after: true
+      }
     ],
     // vue
-    "vue/singleline-html-element-content-newline": "off",
-    "template-curly-spacing": "off",
-    "vue/component-name-in-template-casing": [
-      "warn",
-      "PascalCase",
+    'vue/singleline-html-element-content-newline': 'off',
+    'template-curly-spacing': 'off',
+    'vue/component-name-in-template-casing': [
+      'warn',
+      'PascalCase',
       {
-        registeredComponentsOnly: false,
-      },
+        registeredComponentsOnly: false
+      }
     ],
-    "vue/attributes-order": [
-      "warn",
+    'vue/attributes-order': [
+      'warn',
       {
-        alphabetical: true,
-      },
+        alphabetical: true
+      }
     ],
-    "vue/max-attributes-per-line": [
-      "warn",
+    'vue/max-attributes-per-line': [
+      'warn',
       {
         singleline: 1,
         multiline: {
           max: 1,
-          allowFirstLine: false,
-        },
-      },
+          allowFirstLine: false
+        }
+      }
     ],
-    "vue/script-indent": [
-      "warn",
+    'vue/script-indent': [
+      'warn',
       2,
       {
         baseIndent: 0,
         switchCase: 1,
-        ignores: [],
-      },
+        ignores: []
+      }
     ],
-    "vue/html-indent": [
-      "warn",
+    'vue/html-indent': [
+      'warn',
       2,
       {
         attribute: 1,
         closeBracket: 0,
         alignAttributesVertically: true,
-        baseIndent: 0,
-      },
+        baseIndent: 0
+      }
     ],
-    "@typescript-eslint/no-non-null-assertion": 0,
+    '@typescript-eslint/no-non-null-assertion': 0
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
-        jest: true,
-      },
-    },
-  ],
-};
+        jest: true
+      }
+    }
+  ]
+}
