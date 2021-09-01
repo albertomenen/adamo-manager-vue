0addfd5d8390a8e2a32a4e4bbc7b092ae2d4334a
Javier Martinez <enriquejavier_21@hotmail.com>
2021-09-01 14:57:50 -0400

    Applayout con estilos finalizados

---
+++ src/layouts/app-layout/AppLayout.ts
@@ -0,0 +1,7 @@
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class AppLayout extends Vue {
+
+  open = true
+}
---
+++ src/layouts/app-layout/AppLayout.vue
@@ -0,0 +1,145 @@
+<template>
+<div class="sidebar-page">
+  <section class="sidebar-layout">
+    <BSidebar
+      :fullheight="true"
+      icon="fas"
+      mobile="reduce"
+      open
+      position="static"
+      type="is-medium-blue"
+    >
+      <div class="">
+        <div class="block sidebar-block has-text-centered m-0">
+          <img
+            alt=""
+            class="sidebar-logo p-2"
+            src="@/assets/logo_grupo.svg"
+          />
+        </div>
+        <BMenu class="is-custom-mobile mt-10 pl-12">
+          <BMenuList>
+            <BMenuItem
+              icon="hospital"
+              icon-pack="fas"
+              :label="$tc('spaces.num', 2)"
+              size="is-medium"
+            />
+            <BMenuItem
+              icon="notes-medical"
+              icon-pack="fas"
+              :label="$tc('data.num', 2)"
+              size="is-medium"
+            />
+            <BMenuItem
+              icon="hospital-user"
+              icon-pack="fas"
+              :label="$tc('patients.num', 2)"
+              size="is-medium"
+            />
+          </BMenuList>
+        </BMenu>
+      </div>
+    </BSidebar>
+
+    <div class="content-section">
+      <header class="has-text-white has-background-blue py-2 px-4 is-flex is-justify-content-space-between is-align-items-center navbar">
+        <div>
+          <span class="is-size-7">
+            powered by&nbsp;
+          </span>
+          <img
+            alt="adamo-logo"
+            src="@/assets/logo-white.png"
+            style="height: 18px;"
+          >
+        </div>
+        <div>
+          <BIcon
+            class="is-round has-background-white"
+            icon="user"
+            pack="fas"
+            size="is-medium"
+            type="is-blue"
+          />
+        </div>
+      </header>
+      <div class="pt-6 px-10">
+        <ABreadcrumb
+          textColor="has-text-title"
+        />
+        <div class="page-content is-fullheigth mt-6">
+          <RouterView />
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+</template>
+
+<script src="./AppLayout.ts" lang="ts"></script>
+
+<style lang="scss">
+@import "~adamo-components/src/assets/styles/colors";
+
+.sidebar-page {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+
+  .sidebar-layout {
+    display: flex;
+    flex-direction: row;
+    min-height: 100vh;
+  }
+}
+.sidebar-block {
+  height: 70px;
+  border-bottom: 1px #0C518C solid;
+
+  .sidebar-logo {
+    height: 70px;
+  }
+}
+.content-section {
+  width: 100%;
+
+  .navbar {
+    height: 70px;
+  }
+}
+.menu {
+  .menu-list li {
+    height: 70px;
+    margin-bottom: 11px;
+
+    .icon-text {
+      color: white;
+      font-weight: 600;
+      font-size: 18px;
+      padding-top: 16px;
+      height: 70px;
+    }
+
+    .icon {
+      top: 10%;
+      margin-left: 15px;
+      margin-right: 15px;
+    }
+  }
+
+  .menu-list a:hover  {
+    color: $blue;
+    height: 70px;
+    border-radius: 35px 0px 0px 35px;
+  }
+
+  .menu-list a.is-active  {
+    background-color: white;
+    color: $blue;
+    height: 70px;
+    border-radius: 35px 0px 0px 35px;
+  }
+}
+</style>
