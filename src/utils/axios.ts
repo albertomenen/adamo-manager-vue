7d327f836eaeac1131861f47d9eb8dd217c07ff0
Javier Martinez <enriquejavier_21@hotmail.com>
2021-09-06 04:47:31 -0400

    Modificacion axios redireccionar a login cuando token expire

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios'
+import router from '@/router'
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_URL
@@ -9,5 +10,21 @@ const token = localStorage.token
 if (token) {
   axiosInstance.defaults.headers.common['Authorization'] = token
 }
+
+axiosInstance.interceptors.response.use(
+  function (response: AxiosResponse<unknown>): AxiosResponse {
+    return response
+  },
+  function (error) {
+    const { data } = error.response
+
+    if (data.message === 'Signature expired. Please log in again.') {
+      localStorage.clear()
+      router.push({ name: 'login' })
+    }
+
+    return Promise.reject(error)
+  }
+)
 
 export { axiosInstance }
