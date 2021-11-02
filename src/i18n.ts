e524a64af39923f3b6b9697ffbc3eae0ecf41a3c
Isidro Gonzalez <igonzalezprs@gmail.com>
2021-11-02 19:36:38 -0400

    Fix de i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -18,7 +18,7 @@ function loadLocaleMessages (): LocaleMessages {
 }
 
 export default new VueI18n({
-  locale: localStorage.appLocale || process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: localStorage.appLocale || process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: localStorage.appLocale || process.env.VUE_APP_I18N_LOCALE || 'es',
+  fallbackLocale: localStorage.appLocale || process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'es',
   messages: loadLocaleMessages()
 })
